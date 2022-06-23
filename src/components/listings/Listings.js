afe8202c0775ba51e38dd7c3c90393437041a751
FluffyKas <maruzs.zso@gmail.com>
2022-06-23 14:15:58 +0200

    style: add markup&basic styles

--- src/components/listings/Listings.js
+++ src/components/listings/Listings.js
@@ -1,5 +1,14 @@
+import jobListings from "../../data/data.json";
+import "./Listings.scss";
+import { SingleListing } from "../singe-listing/SingleListing";
+
 export const Listings = () => {
+
   return (
-    <div>I am the Listings container \o/</div>
+    <div className="listings-container">
+      {jobListings.map(listing => (
+        <SingleListing listing={listing} />
+      ))}
+    </div>
   );
 }
